e01301cdc4510b12a88f8a65dfe4ed64bfa4d63c
yassine <boubakeryass@gmail.co>
2017-12-10 08:21:25 +0100

    hhe

--- public/js/chartsDEUX.js
+++ public/js/chartsDEUX.js
@@ -7,58 +7,6 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-
-var ctx = document.getElementById("myAreaPerk");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 20,
-      pointBorderWidth: 2,
-      data: [getRandomInt(1000,3500)+30000, getRandomInt(1000,3500)+30000, getRandomInt(1000,3500)+30000, getRandomInt(1000,3500)+30000, getRandomInt(1000,3500)+30000, getRandomInt(1000,3500)+35000, getRandomInt(1000,3500)+35000, getRandomInt(1000,3500)+35000, getRandomInt(1000,3500)+35000, getRandomInt(1000,3500)+35000, getRandomInt(1000,5000)+35000, getRandomInt(1000,4500)+35000, getRandomInt(1000,2000)+35000],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 40000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
-      }],
-    },
-    legend: {
-      display: false
-    }
-  }
-});
-
 // -- Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
